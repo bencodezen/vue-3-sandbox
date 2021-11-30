0bfec8ba8cb80ed1ab297961683fa09d3c22c02a
Ben Hong <ben@bencodezen.io>
2021-11-30 15:21:44 -0500

    feature: add 1337 issue from docs

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import Issue1337 from './components/Issue1337.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <Issue1337 />
 </template>
 
 <style>
---
+++ src/components/Issue1337.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+const emit = defineEmits(['update:modelValue'])
+
+const close = () => emit('update:modelValue')
+</script>
+
+<template>
+  <h1>
+    <a href="https://github.com/vuejs/docs/issues/1337" target="_blank">
+      Vue Docs Issue #1337
+    </a>
+    <button @click="close">Close</button>
+  </h1>
+</template>
+
+<style></style>
